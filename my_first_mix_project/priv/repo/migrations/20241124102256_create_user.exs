d12f8d00bf445c9b11f85b748ad36e949fb06096
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2024-11-24 11:37:45 +0100

    populated user migration

--- my_first_mix_project/priv/repo/migrations/20241124102256_create_user.exs
+++ my_first_mix_project/priv/repo/migrations/20241124102256_create_user.exs
@@ -2,6 +2,12 @@ defmodule MyFirstMixProject.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :age, :integer
+      add :insersted_at, :native_datetime
+      add :updated_at, :native_datetime
+    end
   end
 end
